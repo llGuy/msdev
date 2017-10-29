fb82016f433e305a8690fb692e22088eeb54eed0
Brice Rosenzweig <briceguard-codesion@yahoo.com>
2017-10-29 14:03:50 +0000

    kernel: added global descriptor table

--- tutorials/kernel/main.c
+++ tutorials/kernel/main.c
@@ -1,10 +1,13 @@
 //#include "multiboot.h"
 #include "monitor.h"
+#include "gdt.h"
 
 //int kernel_main(multiboot_info_t * mboot_ptr)
 int kernel_main()
 {
     monitor_clear();
     monitor_write( "Hello World!" );
+
+    init_gdt();
     return 0xDEADCAFE;
 }
