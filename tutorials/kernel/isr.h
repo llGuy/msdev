e01b0e2ba4c98bac891442f6962fc1dfa0f509a7
brice <briceguard-codesion@yahoo.com>
2017-11-04 17:12:10 +0000

    Wrote code for irq but not yet working

--- tutorials/kernel/isr.h
+++ tutorials/kernel/isr.h
@@ -10,4 +10,13 @@ typedef struct
     uint32_t eip, cs, eflags, useresp, ss; // pushed by processor
 } __attribute__((packed)) registers_t;
 
+void init_isr();
+
+typedef void (*isr_t)(registers_t);
+void register_interrupt_handler(uint8_t n, isr_t handler);
+
+#define IRQ0 32
+#define IRQ1 33
+
+#define IRQ15 47
 #endif
--- tutorials/kernel/main.c
+++ tutorials/kernel/main.c
@@ -2,7 +2,7 @@
 #include "monitor.h"
 #include "gdt.h"
 #include "idt.h"
-
+#include "timer.h"
 //int kernel_main(multiboot_info_t * mboot_ptr)
 int kernel_main()
 {
@@ -14,5 +14,7 @@ int kernel_main()
 
     asm volatile ( "int $0x3" );
     asm volatile ( "int $0x4" );
+
+    init_timer(50);
     return 0xDEADCAFE;
 }
